728c6c0dd5b49e16b0170991c95380c9b2052173
Berkay Derin <zewodi@gmail.com>
2023-09-17 20:08:35 +0300

    Ders 2

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
 
+// template engine
+app.set('view engine', 'ejs')
+
+// middleware
+app.use(express.static('public'))
+
+// routes
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.status(200).render('index', {
+		page_name: 'index'
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.status(200).render('about', {
+		page_name: 'about'
+	})
 })
 
 const port = 3000

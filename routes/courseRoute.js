3e6ea512e12b7a66cd8810dd9095d4c5e4ad57dd
Berkay Derin <zewodi@gmail.com>
2023-10-05 16:36:29 +0300

    5.10.2023

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,7 +5,11 @@ const courseController = require('../controllers/courseController')
 const roleMiddleware = require('../middlewares/roleMiddleware')
 
 router.post('/', roleMiddleware(['teacher', 'admin']), courseController.createCourse)
+
 router.get('/', courseController.getAllCourses)
 router.get('/:slug', courseController.getCourse)
+
 router.post('/enroll', courseController.enrollCourse)
+router.post('/release', courseController.releaseCourse)
+
 module.exports = router
